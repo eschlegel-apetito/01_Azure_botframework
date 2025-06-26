dffc51f34c872d02eebce5677a05458414a82e38
Eduard Schlegel <eduard.schlegel@apetito.de>
2025-06-26 10:46:38 +0200

    FLASKTEST

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    print("Starting Flask app...")
+@app.route('/')
+def home():
+    return '<h1>✅ Flask läuft erfolgreich auf Azure!</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
