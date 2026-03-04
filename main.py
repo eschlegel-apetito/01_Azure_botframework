da6c2d9c21b4c4e524467f42b5ddc375dce868a0
Eduard Schlegel <eduard.schlegel@apetito.de>
2026-03-04 11:20:11 +0100

    port 8000

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3978))
+    port = int(os.environ.get("PORT", 8000))
+    print(f"Starting server on port {port}...")
     app.run(host="0.0.0.0", port=port)
